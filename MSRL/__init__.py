4e53bf87605aa9ef7aa65761c7db87b1b7cecba0
Geona Kim (Windows_11) <rtguna@icloud.com>
2024-08-30 17:36:14 +0900

    test : HammingScore 측정 지표 클래스 작성.
    기존의 MeanAccuracy 클래스를 없애고 HammingScore 클래스로 대체하여 새로
    작성함. 그리고 이를 사용하도록 코드도 수정.

--- MSRL/__init__.py
+++ MSRL/__init__.py
@@ -3,7 +3,9 @@ from .datasets import MusicalSymbolDataset
 from .models import MusicalSymbolModel
 from .losses import WeightedBinaryCrossentropy
 from .metrics import Accuracy
-from .metrics import MeanAccuracy
+from .metrics import HammingScore
+from .metrics import Precision
+from .metrics import Recall
 
 # 패키지 버전
 VERSION = 1.0
--- MSRL/metrics.py
+++ MSRL/metrics.py
@@ -41,42 +41,53 @@ class Accuracy(tf.keras.metrics.Metric):
         base_config = super(Accuracy, self).get_config()
         return {**base_config}
     
-# 정확도
-class MeanAccuracy(tf.keras.metrics.Metric):
+# 해밍 점수
+class HammingScore(tf.keras.metrics.Metric):
     """
-    다중 레이블 y_true와 y_pred를 입력받아 정확도를 계산함.
+    다중 레이블 y_true와 y_pred를 입력받아 해밍점수를 계산함.
 
-    각각의 레이블 인덱스 별로 정확도를 구하고 평균적인 정확도를 구해줌.
+    올바르게 분류된 레이블의 비율을 구하는 방식.
     """
-    def __init__(self, num_classes, name='MeanAccuracy', **kwargs):
-        super(MeanAccuracy, self).__init__(name=name, **kwargs)
-        # 레이블 클래스 갯수
-        self.num_classes = num_classes
-        # 전체 샘플 수(total), 올바르게 분류된 레이블 수(count)
+    def __init__(self, name='HammingScore', **kwargs):
+        super(HammingScore, self).__init__(name, **kwargs)
+        # 레이블이 올바르게 분류된 비율 합산(total), 올바르게 분류된 레이블 수(count)
         self.total = self.add_weight("total", initializer='zeros')
-        self.count = self.add_weight("count", shape=(num_classes,), initializer='zeros')
-        
+        self.count = self.add_weight("count", initializer='zeros')
+
     def update_state(self, y_true, y_pred, sample_weight=None):
         # 자료형 통일
         y_true = tf.cast(y_true, dtype=y_pred.dtype)
-        # y_pred 예측값 제한
+        # y_pred 예측값 반올림
         y_pred = tf.round(y_pred)
-        # y_true == y_pred 확인
+        # 올바르게 분류된 비율 계산
         metric = tf.equal(y_true, y_pred)
         metric = tf.cast(metric, dtype=tf.float32)
         # 확인 결과 저장(누적)
-        self.total.assign_add(tf.cast(tf.shape(metric)[0], dtype=tf.float32))
-        self.count.assign_add(tf.reduce_sum(metric, axis=-2))
-        
+        self.count.assign_add(tf.reduce_sum(metric))
+        self.total.assign_add(tf.cast(tf.size(metric), dtype=tf.float32))
+    
     def result(self):
-        # 정확도 계산
-        return tf.reduce_mean(self.count / self.total)
+        return self.count / self.total
     
     def reset_state(self):
         # 누적 변수 초기화
         self.total.assign(0)
-        self.count.assign(tf.zeros(shape=(self.num_classes,), dtype=tf.float32))
+        self.count.assign(0)
         
-    def get_config(self):
-        base_config = super(MeanAccuracy, self).get_config()
-        return {**base_config, 'num_classes':int(self.num_classes)}
+# 정밀도
+class Precision(tf.keras.metrics.Metric):
+    """
+    다중 레이블 y_true와 y_pred를 입력받아 정밀도를 계산함.
+
+    레이블중 하나라도 틀리면 해당 레이블 전체가 틀리것으로 간주하여 채점함.
+    """
+    pass
+
+# 재현율
+class Recall(tf.keras.metrics.Metric):
+    """
+    다중 레이블 y_true와 y_pred를 입력받아 재현율을 계산함.
+
+    레이블중 하나라도 틀리면 해당 레이블 전체가 틀리것으로 간주하여 채점함.
+    """
+    pass
