8e805c3ad270ef259d8ed561e35cf8b754c99cfc
Geona Kim (Windows_11) <rtguna@icloud.com>
2024-09-01 14:10:37 +0900

    test : WeightedBinaryCrossentropy() 손실 클래스 수정.
    클래스 비율 별로 가중치 정도가 계산되는 것이 아닌, 각 클래스를 나타내는
    레이블 마다 0과 1의 비율을 이용하여 가중치 정도를 계산하도록 코드를
    수정함.

--- MSRL/losses.py
+++ MSRL/losses.py
@@ -4,35 +4,37 @@ import tensorflow as tf             # 텐서플로
 
 # 사용자 손실 함수
 class WeightedBinaryCrossentropy(tf.keras.losses.Loss):
-    def __init__(self, numbers, alpha=1.0, **kwargs):
-        super(WeightedBinaryCrossentropy, self).__init__(**kwargs)
-        # 전체 합
-        total = sum(numbers)
-        # 자신의 클래스가 차지하는 비중(비율) 구하기
-        self.rate = [n / total for n in numbers]
-        # 범위 제한
-        epsilon = tf.keras.backend.epsilon()    
-        self.rate = tf.clip_by_value(self.rate, epsilon, 1.0-epsilon)
-        # 비율이 작은것이 큰 가중치를 가지도록 로그함수 통과
-        self.rate = 1.0 - tf.math.log(self.rate)
+    """
+    소수 레이블에 더 많은 가중치를 부과하는 손실 함수.
+    """
+    def __init__(self, total_count, class_count, name='WeightedBinaryCrossentropy', **kwargs):
+        super(WeightedBinaryCrossentropy, self).__init__(name=name, **kwargs)
+        # 가중치 계산
+        self.weight_true = [n / total_count for n in class_count]
+        self.weight_false = [1.0 - n for n in self.weight_true]
+        # 가중치 범위 제한 (0+epsilon <= w <= 1)
+        epsilon = tf.keras.backend.epsilon()
+        self.weight_true = tf.clip_by_value(self.weight_true, epsilon, 1.0-epsilon)
+        self.weight_false = tf.clip_by_value(self.weight_false, epsilon, 1.0-epsilon)
+        # 가중치 로그함수 처리
+        self.weight_true = 1.0 - tf.math.log(self.weight_true)
+        self.weight_false = 1.0 - tf.math.log(self.weight_false)
+
 
     def call(self, y_true, y_pred):
         # 자료형 통일
         y_true = tf.cast(y_true, dtype=y_pred.dtype)
-        # y_pre 범위 제한(epsilon ~  (1.0 - epsilon))
+        # y_pre 범위 제한 (0+epsilon <= y_pred <= 1-epsilon)
         epsilon = tf.keras.backend.epsilon()
         y_pred = tf.clip_by_value(y_pred, epsilon, 1.0-epsilon)
-        # Binary Crossentropy 손실 계산
+        # Binary Crossentropy loss 계산
         loss = -(y_true * tf.math.log(y_pred) + (1-y_true) * tf.math.log(1-y_pred))
-        # 배치 크기 계산
-        batch_size = tf.shape(y_pred)[0]
-        # 계산된 가중치 브로드 개스팅
-        weights = tf.broadcast_to(self.rate, [batch_size, tf.shape(self.rate)[0]])
-        # 가중치를 사용하여 손실을 계산함
-        weighted_losses = tf.reduce_sum(weights * loss, axis=-1)
+        # loss * weight
+        weighted_losses = tf.where(y_true==1.0, loss * self.weight_true, loss * self.weight_false)
+        weighted_losses = tf.reduce_sum(weighted_losses, axis=-1)
         weighted_losses = tf.reduce_mean(weighted_losses)
         return weighted_losses
     
     def get_config(self):
         base_config = super().get_config()
-        return {**base_config, "rate":self.rate.numpy().tolist()}
+        return {**base_config, "weight_true":self.weight_true.numpy().tolist(), "weight_false":self.weight_false.numpy().tolist()}
